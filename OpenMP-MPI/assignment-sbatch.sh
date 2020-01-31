4fc00c5ebc8237300778761070bb60b298589514
pjotr <pjotrscholtze1@gmail.com>
2020-01-31 12:25:19 +0100

    sbatch file

--- OpenMP-MPI/assignment-sbatch.sh
+++ OpenMP-MPI/assignment-sbatch.sh
@@ -1,6 +1,20 @@
 #!/bin/bash -e
-#SBATCH -t 10:00 -N 1 --mem=100M
-
+#SBATCH -t 2:30 -N 1 -n 4 --mem=100M
 export OMP_NUM_THREADS=`nproc --all`
 
-mpirun ./assignment
+echo ">> [START] Running on 4 cores"
+mpirun -np 4 ./assignment
+echo ">> [STOP] Running on 4 cores"
+
+echo ">> [START] Running on 3 cores"
+mpirun -np 3 ./assignment
+echo ">> [STOP] Running on 3 cores"
+
+echo ">> [START] Running on 2 cores"
+mpirun -np 2 ./assignment
+echo ">> [STOP] Running on 2 cores"
+
+echo ">> [START] Running on 1 cores"
+mpirun -np 1 ./assignment
+echo ">> [STOP] Running on 1 cores"
+
