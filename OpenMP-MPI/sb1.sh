47167c545a44fe513d2720c4e23afe40a11cfa62
pjotr <pjotrscholtze1@gmail.com>
2020-02-04 12:25:23 +0100

    added messages for #defines

--- OpenMP-MPI/sb1.sh
+++ OpenMP-MPI/sb1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 #SBATCH -t 0:10 -N 1 -n 1 --mem=2000M
 
-export OMP_NUM_THREADS=`nproc --all`
-
 echo "#define SIZE_N 23000"
 echo "#define BLOCK_SIZE 100"
 echo "#define R_MULTIPLIER 130"
-
+export OMP_NUM_THREADS=`nproc --all`
+echo "OMP_NUM_THREADS="$OMP_NUM_THREADS
 mpirun -np 1 ./assignment
--- OpenMP-MPI/sb4.sh
+++ OpenMP-MPI/sb4.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 #SBATCH -t 0:10 -N 1 -n 4 --mem=2000M
+echo "#define SIZE_N 23000"
+echo "#define BLOCK_SIZE 100"
+echo "#define R_MULTIPLIER 130"
 
 export OMP_NUM_THREADS=`nproc --all`
 mpirun -np 4 ./assignment
